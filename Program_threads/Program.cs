511db54eea634d26a72196cd62c89fa16a078638
szmitek123 <100719876+szmitek123@users.noreply.github.com>
2022-04-11 20:02:20 +0200

    Dodaj pliki projektów.

---
+++ Program_threads/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Program_threads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            Test test = new Test();
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            Thread[] threads = new Thread[n];
+            for (int i = 0; i < n; i++)
+            {
+                var temp = i;
+                threads[i] = new Thread(() => test.set(temp));
+            }
+            for (int i = 0; i < n; i++)
+            {
+                threads[i].Start();
+            }
+            for (int i = 0; i < n; i++)
+                threads[i].Join();
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine(elapsedMs + " ms");
+            foreach (double x in test.array)
+                Console.WriteLine(x + " ");
+            Console.Read();
+        }
+        public class Test
+        {
+            public double[] array = new double[10];
+            public void set(int i)
+            {
+                Thread.Sleep(500);
+                double x = Math.Pow((int)i, 2.0f);
+                array[(int)i] = x;
+            }
+        }
+    }
+}
---
+++ lab3_mnozenie_macierzy/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace lab3_mnozenie_macierzy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr1 = new int[10, 10];
+            int[,] arr2 = new int[10, 10];
+            int[,] arr3 = new int[10, 10];
+            int liczba_watkow = 10;
+            Thread[] threads = new Thread[liczba_watkow];
+
+            
+                Random r = new Random();
+
+                for (int ki = 0; ki < 10; ki++)
+                {
+                    for (int kj = 0; kj < 10; kj++)
+                    {
+                        arr1[ki, kj] = r.Next(1, 20);
+                    }
+                }
+
+                for (int ki = 0; ki < 10; ki++)
+                {
+                    for (int kj = 0; kj < 10; kj++)
+                    {
+                        arr2[ki, kj] = r.Next(1, 100);
+                    }
+                }
+            
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    int s = 0;
+                    for (int k = 0; k < 10; k++)
+                    {
+                        s += arr1[i, k] * arr2[k, j];
+                    }
+                    arr3[i,j] = s;
+                }
+            }
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(arr1[i,j]+" ");
+                }
+                Console.WriteLine("\n");
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(arr2[i, j] + " ");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("Macierz wynikowa: \n");
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(arr3[i, j] + " ");
+                }
+                Console.WriteLine("\n");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
